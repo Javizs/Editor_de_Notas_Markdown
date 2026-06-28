90639a2c7b9fd1e15962a4b9fff56fe4236c7b38
Javi <javier.s396756@cesurformacion.com>
2026-06-28 22:38:25 +0200

    Logica de guardado corregida y optimizada con arrays, boton de nueva nota y eliminar con lógica

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const tituloNota = document.querySelector("#editorNotasNombre");
 const textoNota = document.querySelector("#editorNotas");
 const btnGuardarNota = document.querySelector("#btnGuardarNota");
+const btnNuevaNota = document.querySelector("#btnNuevaNota");
+const btnEliminarNota = document.querySelector("#btnEliminarNota");
+let notaSeleccionadaId =null;
 
 btnGuardarNota.addEventListener("click",() => {
 
@@ -8,20 +11,87 @@ const titulo = tituloNota.value.trim();
 const texto = textoNota.value.trim();
 
 if (titulo === "" || texto === ""){
-    showMessage("La nota está vacía")
+    alert("La nota está vacía");
     return;
 }
 
-const nuevaNota = {
-    id: Date.now,
+
+const notas = JSON.parse(localStorage.getItem("notas")) || [];
+if(notaSeleccionadaId === null){
+    const nuevaNota = {
+    id: Date.now(),
     titulo: titulo,
     texto:texto,
-    fecha: new Date().toISOString
+    fecha: new Date().toISOString()
 
 };
+notas.push(nuevaNota);
+} else {
+    const notaEncontrada = notas.find((nota) => nota.id === notaSeleccionadaId);
+    if(notaEncontrada){
+        notaEncontrada.titulo =titulo;
+        notaEncontrada.texto = texto;
+        notaEncontrada.fecha = new Date().toISOString();
+    }
+}
 
-localStorage.setItem("nota", JSON.stringify(nuevaNota));
+localStorage.setItem("notas", JSON.stringify(notas));
 
-showMessage("Nota Guardada correctamente");
-
+alert("Nota Guardada correctamente");
+mostrarNotas();
 })
+
+
+// Lógica para mostrar el array en mis notas 
+
+const listaNotas = document.querySelector("#listaNotas");
+
+function mostrarNotas(){
+    const notas = JSON.parse(localStorage.getItem("notas")) || [];
+
+    listaNotas.innerHTML ="";
+
+    notas.forEach((nota) => {
+        const recuadroNota = document.createElement("div");
+        recuadroNota.classList.add("notaGuardada")
+        const titulo = document.createElement("h3");
+        titulo.textContent = nota.titulo;
+        recuadroNota.appendChild(titulo);
+        listaNotas.appendChild(recuadroNota);
+
+        recuadroNota.addEventListener("click",() =>{
+            tituloNota.value = nota.titulo;
+            textoNota.value = nota.texto;
+            notaSeleccionadaId = nota.id;
+        });
+        listaNotas.appendChild(recuadroNota);
+    });
+
+}
+    mostrarNotas();
+
+//BOTON NUEVA NOTA
+    btnNuevaNota.addEventListener("click",() =>{
+        tituloNota.value = "";
+        textoNota.value = "";
+        notaSeleccionadaId = null;
+    })
+
+//BOTON ELIMINAR NOTA 
+
+btnEliminarNota.addEventListener("click", () => {
+    if (notaSeleccionadaId === null){
+        alert("Seleccione una nota para eliminar");
+        return;
+    }
+    const notas = JSON.parse(localStorage.getItem("notas")) || [];
+    const NotasFiltradas = notas.filter((nota) => nota.id !== notaSeleccionadaId);
+    
+    localStorage.setItem ("notas",JSON.stringify(NotasFiltradas));
+    tituloNota.value ="";
+    textoNota.value = "";
+    notaSeleccionadaId.value = null;
+
+    mostrarNotas();
+    alert("Nota eliminada correctamente"); 
+});
